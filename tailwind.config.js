cd429b8365064c7cb23a6847c7d2fa6ae97cfb3e
HiroyukiUrata <hilloween1031@gmail.com>
2023-04-06 11:35:57 +0900

    111

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js1,jsx1,ts1,tsx1}",
   ],
   theme: {
     extend: {},
